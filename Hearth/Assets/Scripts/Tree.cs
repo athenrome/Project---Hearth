5dbe7f4f95a73474851fff5c56c32379863f18d0
Matt <bfc.shbai@gmail.com>
2016-08-09 11:53:08 +1000

    Added: ground textures (PlaceHolder [PH]), Log Seats (PH), Firepit Texture (PH), Light source x3, Tree script Placed trees and log seats.

--- Hearth/Assets/Scripts/Tree.cs
+++ Hearth/Assets/Scripts/Tree.cs
@@ -3,24 +3,34 @@ using System.Collections;
 
 public class Tree : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public float scaleX = 0.6485662f;
+    public float scaleY = 1.040526f;
+    public float scaleZ = 0.9673969f;
+    public float scaleMod;
+
+    // Use this for initialization
+    void Start ()
+    {
+        scaleMod = Random.Range(0.5f, 1.0f);
+        scaleX += scaleMod;
+        scaleY += scaleMod;
+        scaleZ += scaleMod;
         ScaleTree();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    
 	}
 
     public void ScaleTree ()
     {
-        float scale = Random.Range(0.7f, 2.0f);
-        transform.localScale=new Vector3(scale, scale, scale);
+        transform.localScale=new Vector3(scaleX, scaleY, scaleZ);
         transform.eulerAngles = new Vector3(-90.0f, Random.Range(0.0f, 360.0f), 0.0f);
-        Bounds b = transform.GetComponent<Collider>().GetComponent<Bounds>();
-        float offset = b.min.z*scale;
-       // Vector3 pos = transform.position;
+        //Bounds b = transform.GetComponent<Collider>().GetComponent<Bounds>();
+        //float offset = b.min.z*scale;
+        //Vector3 pos = transform.position;
         //pos.y = offset;
         //ransform.position = pos;
 
