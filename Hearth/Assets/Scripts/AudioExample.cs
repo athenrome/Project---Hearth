7cf7f9721ca92f6f0c264652f8c624361efab758
Matt <bfc.shbai@gmail.com>
2016-08-12 15:23:28 +1000

    created a new scene to test hacky stuff in

--- Hearth/Assets/Scripts/AudioExample.cs
+++ Hearth/Assets/Scripts/AudioExample.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class AudioExample : MonoBehaviour {
 
+    [FMODUnity.EventRef]
+    public string atmoSound = "event:/Atmos/Atmos";
+
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+	    
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    
 	}
+
 }
