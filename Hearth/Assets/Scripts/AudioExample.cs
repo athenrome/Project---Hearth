fb2aed534a466a59838c93fd6696bd89008cac1a
Matt <bfc.shbai@gmail.com>
2016-08-12 22:08:33 +1000

    random meta garbage

--- Hearth/Assets/Scripts/AudioExample.cs
+++ Hearth/Assets/Scripts/AudioExample.cs
@@ -5,13 +5,17 @@ public class AudioExample : MonoBehaviour {
 
     [FMODUnity.EventRef]
     public string atmoSound = "event:/Atmos/Atmos";
-    FMOD.Studio.EventInstance AtmoStartEvent;
+    public string textTypeSound = "event:/Player/Speech_Bubble";
+    FMOD.Studio.EventInstance TalkStartEvent;
+
+
 
 	// Use this for initialization
 	void Start ()
     {
-        //FMODUnity.RuntimeManager.PlayOneShot(atmoSound);
-        AtmoStartEvent = FMODUnity.RuntimeManager.CreateInstance(atmoSound);
+        FMODUnity.RuntimeManager.PlayOneShot(atmoSound);
+        TalkStartEvent = FMODUnity.RuntimeManager.CreateInstance(textTypeSound);
+
     }
 	
 	// Update is called once per frame
