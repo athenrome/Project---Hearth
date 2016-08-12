adf6b69ffc9131d119d011482a7a07d97d00d4b1
Matt <bfc.shbai@gmail.com>
2016-08-12 17:23:48 +1000

    messing with audio

--- Hearth/Assets/Scripts/AudioExample.cs
+++ Hearth/Assets/Scripts/AudioExample.cs
@@ -5,13 +5,14 @@ public class AudioExample : MonoBehaviour {
 
     [FMODUnity.EventRef]
     public string atmoSound = "event:/Atmos/Atmos";
-
+    FMOD.Studio.EventInstance AtmoStartEvent;
 
 	// Use this for initialization
 	void Start ()
     {
-	    
-	}
+        //FMODUnity.RuntimeManager.PlayOneShot(atmoSound);
+        AtmoStartEvent = FMODUnity.RuntimeManager.CreateInstance(atmoSound);
+    }
 	
 	// Update is called once per frame
 	void Update ()
