556a1acae334f5dae7db589bda9c086a11da5d84
Athenrome <athenrome@gmail.com>
2016-08-13 22:27:34 +1000

    Updated GameFloor script to instantly return wood

---
+++ Hearth/Assets/Scripts/GameFloor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameFloor : MonoBehaviour
+{
+    public WoodPile pile;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<Wood>() == true)
+        {
+            pile.woodCount--;
+            pile.AddWood(1);
+            GameObject.Destroy(collision.gameObject);
+        }
+    }
+}
+
