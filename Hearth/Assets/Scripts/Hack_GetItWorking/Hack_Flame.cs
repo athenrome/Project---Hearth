a928cde621f37dbf6aa2f18d131a9d130652b507
Matt <bfc.shbai@gmail.com>
2016-08-13 17:22:21 +1000

    made the fire go up and down depending on if there is wood, (press space to add wood, it lasts for 5 sec right now) really messy, but functional.

--- Hearth/Assets/Scripts/Hack_GetItWorking/Hack_Flame.cs
+++ Hearth/Assets/Scripts/Hack_GetItWorking/Hack_Flame.cs
@@ -5,29 +5,38 @@ public class Hack_Flame : MonoBehaviour {
 
     public float minLevel;
     public float maxLevel;
+    public float FogminLevel = 0.01f;
+    public float FogmaxLevel = 0.12f;
 
     public Light lightSource;
 
     public ParticleSystem fireParticles;
     
-    [Range(0f, 100f)]
+    [Range(0f, 8f)]
     public float intensity; //flame intensity target
 
-    
+    public float logBurnTime = 0;
+    public float maxBurnTime = 5;
+    bool logActive = false;
+
 
     //bool actionInProgress;
-    FlameAction currAction;
+    //FlameAction currAction;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         //lightSource.range = minLevel;
+
+        //testing intensity
+        intensity = 8;
 	
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        StokeFire();
         IntensityModifier();
         //RangeModifier();
     }
@@ -48,9 +57,25 @@ public class Hack_Flame : MonoBehaviour {
     */
     void IntensityModifier()
     {
-        intensity = (Time.deltaTime / 2);
+        if (logActive == false)
+        {
+            intensity -= (Time.deltaTime / 10);
+        }
 
-        if (lightSource.intensity < intensity)
+        if (logActive == true)
+        {
+            logBurnTime -= Time.deltaTime;
+            intensity += (Time.deltaTime / 5);
+
+            if(logBurnTime <= 0)
+            {
+                logActive = false;
+            }
+
+
+        }
+
+        if (lightSource.intensity <= intensity)
         {
             lightSource.intensity += Time.deltaTime;
         }
@@ -58,6 +83,24 @@ public class Hack_Flame : MonoBehaviour {
         if (lightSource.intensity > intensity)
         {
             lightSource.intensity -= Time.deltaTime;
+        }
+
+        //clamp intensity so it doesnt go way out of range.
+        Mathf.Clamp(intensity, 0.0f, 8.0f);
+                                                            //Debug.Log("clamped");
+    }
+
+    void StokeFire()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+                                                            Debug.Log("add log");
+
+            logActive = true;
+            logBurnTime = maxBurnTime;
+
+                                                            Debug.Log("log added, time till burn = ");
+            
         }
     }
 
--- Hearth/Assets/Scripts/Hack_GetItWorking/LightFlicker.cs
+++ Hearth/Assets/Scripts/Hack_GetItWorking/LightFlicker.cs
@@ -10,22 +10,35 @@ public class LightFlicker : MonoBehaviour {
     //public float flickerPercentage = 0.1f;
     public float targetIntensity = 1;
 
-    public Flame flame;
+    //the value used to offset one flames brightness from another. this should be set to 0 on one, and # on the other flame in the inspector.
+    public float offset;
+
+
+    //restore this after fixing the hack... should use flame instead of hack flame
+    //public Flame flame;
+    
+    //Hacking>>>>>>>>>>>>>>>>
+    public Hack_Flame flame;
+
+    //Hacking End>>>>>>>>>>>>>
 
 
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-
-        flame = FindObjectOfType<Flame>();
-
+        //restore this after fixing the hack... should use flame instead of hack flame
+        //flame = FindObjectOfType<Flame>();
+        flame = FindObjectOfType<Hack_Flame>();
         myLight = GetComponent<Light>();
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        targetIntensity = flame.intensity - offset;
+
         counter += Time.deltaTime;
         if(counter > rate)
         {
