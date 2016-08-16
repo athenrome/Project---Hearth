637df95fab5b80ef217ba63ba97788fa7b1e754a
Athenrome <athenrome@gmail.com>
2016-08-17 02:02:36 +1000

    Solved issue where new dialogue would start halfway another characters dialogue

--- Hearth/Assets/Scripts/CharacterController.cs
+++ Hearth/Assets/Scripts/CharacterController.cs
@@ -81,7 +81,7 @@ public class CharacterController : MonoBehaviour {
 
     public void Speak(DialogueType toSpeak, bool forceSpeak)
     {
-        if (!isActiveAndEnabled || !diagWin.finished)
+        if (!isActiveAndEnabled )//|| !diagWin.finished)
             return;
 
         if (director.canSpeak == true || forceSpeak == true)//if none else is speaking
